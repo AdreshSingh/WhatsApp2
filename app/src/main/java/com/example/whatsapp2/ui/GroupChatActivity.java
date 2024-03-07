77f0db99129cff41a8afb5585e16eb8a29a8b9e5
Devank Singh <adreshsingh271527@gmail.com>
2024-03-07 14:08:27 +0530

    added group chat feature

---
+++ app/src/main/java/com/example/whatsapp2/ui/GroupChatActivity.java
@@ -0,0 +1,76 @@
+package com.example.whatsapp2.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.example.whatsapp2.Adapters.ChatAdapter;
+import com.example.whatsapp2.MainActivity;
+import com.example.whatsapp2.Models.MessageModel;
+import com.example.whatsapp2.R;
+import com.example.whatsapp2.databinding.ActivityGroupChatBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class GroupChatActivity extends AppCompatActivity {
+    ActivityGroupChatBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityGroupChatBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        
+        binding.appBackArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(GroupChatActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        
+        final FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        final ArrayList<MessageModel> messageModels = new ArrayList<>();
+        
+        final String senderId = FirebaseAuth.getInstance().getUid();
+        
+        binding.appUserName.setText(getString(R.string.group_chat));
+        
+        final ChatAdapter adapter = new ChatAdapter(messageModels,this);
+        binding.appChatRecyclerView.setAdapter(adapter);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        binding.appChatRecyclerView.setLayoutManager(linearLayoutManager);
+        
+        binding.appSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String message = binding.appMessage.getText().toString();
+                final MessageModel model = new MessageModel(senderId,message);
+                model.setTimeStamp(new Date().getTime());
+                
+                binding.appMessage.setText("");
+                firebaseDatabase.getReference().child("GroupChat")
+                        .push()
+                        .setValue(model)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Toast.makeText(GroupChatActivity.this, "Message send", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
+    }
+}
